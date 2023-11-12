87b450815d11e08db9d1d0fbb19a7a75b40be725
MegaHamer <135736897+MegaHamer@users.noreply.github.com>
2023-11-12 11:58:21 +0300

    Починил вилки, сделаки 1 полочку и 2 палочки

--- src/main/kotlin/Filosof.kt
+++ src/main/kotlin/Filosof.kt
@@ -2,15 +2,15 @@ class Filosof(var name:String) {
     var itemsInHands = 0;
     var itemsInLeftHand = 0;
     var itemsInRightHand = 0;
-    fun getFork (rightFilosof: Filosof,leftFilosof: Filosof, mode:Int):Boolean{
+    fun getFork (leftFilosof: Filosof,rightFilosof: Filosof, ForkCount:Int):Boolean{
         var boo = true
         when ((0..1).random()){
             0->{//взять слева
-                if (getLeft(leftFilosof,mode)){
-                    println("Философ ${name} взял вилку справа")
+                if (getLeft(leftFilosof,ForkCount)){
+                    println("Философ ${name} взял вилку СЛЕВА")
                 }
-                else if (getRight(rightFilosof,mode)){
-                    println("Философ ${name} взял вилку слева")
+                else if (getRight(rightFilosof,ForkCount)){
+                    println("Философ ${name} взял вилку СПРАВА")
                 }
                 else{
                     boo = false
@@ -18,10 +18,10 @@ class Filosof(var name:String) {
                 }
             }
             1->{//взять справа
-                if (getRight(rightFilosof,mode)){
+                if (getRight(rightFilosof,ForkCount)){
                     println("Философ ${name} взял вилку справа")
                 }
-                else if (getLeft(leftFilosof,mode)){
+                else if (getLeft(leftFilosof,ForkCount)){
                     println("Философ ${name} взял вилку слева")
                 }
                 else {
@@ -32,7 +32,7 @@ class Filosof(var name:String) {
         }
         return boo
     }
-    fun getStick (rightFilosof: Filosof,leftFilosof: Filosof, stickCount:Int=1):Boolean{
+    fun getStick (leftFilosof: Filosof,rightFilosof: Filosof, stickCount:Int=1):Boolean{
         var boo = true
         var rnd = (0..1).random();
         for (i in 0..1){
@@ -53,10 +53,10 @@ class Filosof(var name:String) {
             println("Философ ${name} взял палкочку слева и справа")
         }
         else if (itemsInLeftHand == 1){
-            println("Философ ${name} взял палкочку справа")
+            println("Философ ${name} взял палкочку слева")
         }
         else if (itemsInRightHand == 1){
-            println("Философ ${name} взял палкочку слева")
+            println("Философ ${name} взял палкочку справа")
         }
         else if (itemsInHands ==0){
             println("Философ ${name} призадумался")
@@ -65,35 +65,39 @@ class Filosof(var name:String) {
         //println("Философ ${name}  справа ${itemsInRightHand}  слева ${itemsInLeftHand}  всего ${itemsInHands}")
         return boo
     }
-    fun getSticks (rightFilosof: Filosof,leftFilosof: Filosof, stickCount:Int=2):Boolean{
+    fun getSticks (leftFilosof: Filosof,rightFilosof: Filosof, stickCount:Int=2):Boolean{
         var boo = true
         for (i in 0..1){
             var rnd = (0..1).random();
             when (rnd){
                 0->{//левая
-                    getLeft(leftFilosof,stickCount)
+                    if (!getLeft(leftFilosof,stickCount)){
+                        getRight(rightFilosof,stickCount)
+                    }
                     //println(" левый Философ ${leftFilosof.name}  справа ${leftFilosof.itemsInRightHand}  слева ${leftFilosof.itemsInLeftHand}  всего ${leftFilosof.itemsInHands}")
                 }
                 1->{//правая
-                    getRight(rightFilosof,stickCount)
+                    if (!getRight(rightFilosof,stickCount)){
+                        getLeft(leftFilosof,stickCount)
+                    }
                     // println(" правый Философ ${rightFilosof.name}  справа ${rightFilosof.itemsInRightHand}  слева ${rightFilosof.itemsInLeftHand}  всего ${rightFilosof.itemsInHands}")
                 }
             }
         }
         if ( itemsInLeftHand == 2){
-            println("Философ ${name} взял палкочки справа")
+            println("Философ ${name} взял 2 палкочки слева")
         }
         else if (itemsInRightHand == 2){
-            println("Философ ${name} взял палкочки слева")
+            println("Философ ${name} взял 2 палкочки справа")
         }
         else if (itemsInRightHand == 1 && itemsInLeftHand ==1){
             println("Философ ${name} взял палкочки слева и справа")
         }
         else if (itemsInRightHand == 1 ){
-            println("Философ ${name} взял палкочку слева")
+            println("Философ ${name} взял 1 палкочку справа")
         }
         else if ( itemsInLeftHand == 1){
-            println("Философ ${name} взял палкочку справа")
+            println("Философ ${name} взял 1 палкочку слева")
         }
         else if (itemsInHands ==0){
             println("Философ ${name} призадумался")
@@ -102,16 +106,20 @@ class Filosof(var name:String) {
         //println("Философ ${name}  справа ${itemsInRightHand}  слева ${itemsInLeftHand}  всего ${itemsInHands}")
         return boo
     }
-    private fun getLeft (naiborFilosof: Filosof, count:Int):Boolean{
-        if (naiborFilosof.itemsInRightHand + this.itemsInLeftHand <count){
+    private fun getLeft (leftFilosof: Filosof, count:Int):Boolean{
+        println(" левый Философ ${leftFilosof.name}  справа ${leftFilosof.itemsInRightHand}  слева ${leftFilosof.itemsInLeftHand}  всего ${leftFilosof.itemsInHands}")
+        println(leftFilosof.itemsInRightHand + this.itemsInLeftHand <count)
+        if (leftFilosof.itemsInRightHand + this.itemsInLeftHand <count){
             this.itemsInLeftHand++
             this.itemsInHands++
             return true
         }
         else return false
     }
-    private fun getRight (naiborFilosof: Filosof, count:Int):Boolean{
-        if (naiborFilosof.itemsInLeftHand + this.itemsInRightHand < count){
+    private fun getRight (rightFilosof: Filosof, count:Int):Boolean{
+        println(" правый Философ ${rightFilosof.name}  справа ${rightFilosof.itemsInRightHand}  слева ${rightFilosof.itemsInLeftHand}  всего ${rightFilosof.itemsInHands}")
+        println(rightFilosof.itemsInLeftHand + this.itemsInRightHand <count)
+        if (rightFilosof.itemsInLeftHand + this.itemsInRightHand < count){
             this.itemsInRightHand++
             this.itemsInHands++
             return true
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,38 +10,44 @@ fun main() {
         println("Это не число");
         exitProcess(1)
     }
+    while (true){
     var table:Array<Filosof> = arrayOf()
     for (i in 0 until count){
         print("Введите имя для философа: ")
-        table += Filosof (readln())
-        if (table[i].name == ""){
+        table += Filosof ((i+1).toString())
+        /*if (table[i].name == ""){
             table[i].name = (i+1).toString();
-        }
+        }*/
     }
+
     val order = (0 until  count).shuffled()
     println("Выберите способ:")
     println("1.Вилки")
     println("2.Палочка")
     println("3.Палочка")
-    when (readln()){
-        "1"->{//вилки
-            for (i in order){
-                table[i].getFork(table[(i+count-1)%count],table[(i+count+1)%count],1)
+    when (readln()) {
+        "1" -> {//вилки
+            for (i in order) {
+                table[i].getFork(table[(i + count - 1) % count], table[(i + count + 1) % count], 1)
             }
         }
-        "2"->{//палка
-            for (i in order){
-                table[i].getStick(table[(i+count-1)%count],table[(i+count+1)%count],1)
+
+        "2" -> {//палка
+            for (i in order) {
+                table[i].getStick(table[(i + count - 1) % count], table[(i + count + 1) % count], 1)
             }
         }
-        "3"->{//палки
-            for (i in order){
-                table[i].getSticks(table[(i+count-1)%count],table[(i+count+1)%count],2)
+
+        "3" -> {//палки
+            for (i in order) {
+                table[i].getSticks(table[(i + count - 1) % count], table[(i + count + 1) % count], 2)
             }
         }
-        else->{
+
+        else -> {
             println("Нет такого варианта")
             exitProcess(1)
         }
+    }
     }
 }
